393ae62a97a929b48a13c490d4316528e079c541
Ahmed abdulkareem <ahmedabdalkaremm@gmail.com>
2025-03-11 00:20:20 +0500

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from 'react';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import HomeCards from './components/HomeCards';
+import JobListings from './components/JobListings';
+import ViewAllJobs from './components/ViewAllJobs';
+
+
 
 const App = () => {
-  return (
-    <div>App</div>
+  return (  
+    <>
+    <Navbar />
+    <Hero />
+    <HomeCards />
+    <JobListings />
+    <ViewAllJobs />
+
+
+
+    </>
   )
 }
 
+
+
+
 export default App
+
+
+{/* 
+
+  // To summarize, propse used as a way to pass data from parent to child components.
+
+  // to summarize, states are used to store data that can be changed within a component.
+
+  
+
+  // The return statement is used to render the JSX code to the DOM.
+
+
+*/}
---
+++ src/components/JobListings.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import jobs from '../Jobs.json'
+import JobListing from './JobListing'
+
+const JobListings = () => {
+  const recentJobs = jobs.jobs.slice(0, 3);
+  return (
+
+    <section class="bg-blue-50 px-4 py-10">
+    <div class="container-xl lg:container m-auto">
+      <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">
+        Browse Jobs
+      </h2>
+      <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+
+        {recentJobs.map((job) =>(
+          <JobListing key={job.id} {...
+            job} />
+          
+        ))}
+
+
+
+      </div>
+    </div>
+  </section>
+
+
+  )
+}
+
+export default JobListings
